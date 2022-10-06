ce4d0fc04f463c8e147db4a29d85a06a7064e603
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-10-06 15:37:37 +0300

    Desing_editing

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const nextMusic = () =>{
 
 const playMusic = () =>{
     container.classList.add("playing");
-    play.classList = "fa-solid fa-pause"
+    play.querySelector("i").classList = "fa-solid fa-pause"
 
     audio.play();
 }
@@ -72,7 +72,7 @@ const playMusic = () =>{
 
 const pauseMusic = ()=>{
     container.classList.remove("playing");
-    play.classList = "fa-solid fa-play"
+    play.querySelector("i").classList = "fa-solid fa-play"
     audio.pause();
 }
 
